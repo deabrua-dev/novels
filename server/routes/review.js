63a9385abb0f819c5efafe209bbd901120951696
deabrua-dev <deabrua.co@gmail.com>
2024-06-02 17:33:25 +0300

    Delete not used api

--- server/routes/review.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getReview } from "../controllers/review.js";
-
-const router = express.Router();
-
-router.get("/get/:id", getReview);
-
-export default router;
